dc6736143fac644e0478b04b842ffa8d5c9b564b
Katharina-Krug <58778318+Katharina-Krug@users.noreply.github.com>
2020-04-29 22:19:13 +0200

    Fixing the $_SESSION["lastInput"]
     $_SESSION["lastInput"] = $newentry.$_SESSION["lastInput"];
    
    the .$_SESSION["lastInput"] was missing

--- templates/pages/contactform.tpl.php
+++ templates/pages/contactform.tpl.php
@@ -1,20 +1,18 @@
 <?php
    $re = '/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/';
     //new input
-    if(isset($_POST["fname"]) && isset($_POST["lname"]) && isset($_POST["email"]) && isset($_POST["message"]) && !preg_match($re,$_POST['email']) && empty($_POST['fname'])&& empty($_POST['lname']) && empty($_POST['email']) && empty($_POST['message']))
+    if(isset($_POST["fname"]) && isset($_POST["lname"]) && isset($_POST["email"]) && isset($_POST["message"]) && preg_match($re,$_POST['email']) && !empty($_POST['fname'])&& !empty($_POST['lname']) && !empty($_POST['email']) && !empty($_POST['message']))
     {
-        
-        $newentry = "<br>First name: ".$_POST["fname"]."<br>Last name: ".$_POST["lname"]."<br>Email: ".$_POST["email"]."<br>Message: ".$_POST["message"];
+        $newentry = "<br>First name: ".$_POST["fname"]."<br>Last name: ".$_POST["lname"]."<br>Email: ".$_POST["email"]."<br>Message: ".$_POST["message"]."<br><br>";
         // not first time entering input
        if (isset($_SESSION["lastInput"]) && $_SESSION["lastInput"]!=""){
-            $outout= "<h2>You received the following from the contact form:</h2>".$newentry.$_SESSION["lastInput"];
-            $_SESSION["lastInput"] = $newentry.$_SESSION["lastInput"];
-            
-        }
+                $output = "<h2>You received the following from the contact form:</h2>".$newentry."<h3>Your last messages</h3>".$_SESSION["lastInput"];
+                $_SESSION["lastInput"] = $newentry.$_SESSION["lastInput"];
+            }
         //first time entering input
         else{
             $output = "<h2>You received the following from the contact form:</h2>".$newentry;
-            $_SESSION["lastInput"] = "<br>First name: ".$_POST["fname"]."<br>Last name: ".$_POST["lname"]."<br>Email: ".$_POST["email"]."<br>Message: ".$_POST["message"];
+            $_SESSION["lastInput"] = $newentry.$_SESSION["lastInput"];
         }
 
     }
@@ -37,4 +35,4 @@
 <?php echo $output;?>
 <br><br>
 
-<a href="/Web Programming/Homework/index.php">Back to Home</a>
+<a href="https://www.krugprojects.de">Back to Home</a>
